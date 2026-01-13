f1cc96b9e423b9ed8eb436bc2cdfa8fb7ce8fcec
Emilie Thon <emilie.m.thon@gmail.com>
2026-01-13 10:50:45 +0100

    Added login page to spotify

---
+++ app/login/page.jsx
@@ -0,0 +1,12 @@
+const CLIENT_ID = process.env.CLIENT_ID;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+export default function LoginPage() {
+	return (
+		<a href={`https://accounts.spotify.com/authorize?
+			response_type=code
+			&client_id=${CLIENT_ID}
+			&redirect_uri=${REDIRECT_URI}
+			&show_dialog=true`}>Log in with Spotify</a>
+	);
+}

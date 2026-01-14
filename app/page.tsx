4ca8a121265e71db68aa6a6b0d91a9df8153b13e
Emilie Thon <emilie.m.thon@gmail.com>
2026-01-14 09:18:54 +0100

    Add authentication callback and update page to fetch user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
+import { cookies } from "next/headers";
+
+export default async function Home() {
+  const cookieStore = await cookies();
+  const accessTokenCookie = cookieStore.get("IPM_AT");
+
+  const response = await fetch("https://api.spotify.com/v1/me", {
+    headers: {
+      Authorization: `Bearer ${accessTokenCookie?.value}`
+    }
+  });
+
+  const data = await response.json();
+  console.log(data);
+  
   return null;
 }
